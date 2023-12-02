f29c01e5548bc14ae50a0fe443ff0e8f67d44c54
Aleksei Ponomarenko <lehapon92@gmail.com>
2023-12-02 17:20:41 +0900

    added settings for postgresql

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
+POSTGRES_PASS = os.getenv('PG_ADMIN_PASSWORD')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +81,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_django_hw',
+        'USER': 'postgres',
+        'PASSWORD': POSTGRES_PASS,
     }
 }
 
