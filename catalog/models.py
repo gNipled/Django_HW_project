15b87bb64569350797fa65b8cd72dad7346a7870
Aleksei Ponomarenko <lehapon92@gmail.com>
2023-12-02 18:08:30 +0900

    created models for catalog

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='name')
+    description = models.TextField(verbose_name='description', **NULLABLE)
+    preview = models.ImageField(upload_to='product_img/', verbose_name='product preview', **NULLABLE)
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+    )
+    price = models.FloatField(verbose_name='price per unit')
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='created')
+    change_date = models.DateTimeField(auto_now=True, verbose_name='last Modified')
+
+    def __str__(self):
+        return f'{self.name}, {self.category}, price = {self.price})'
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, verbose_name='name')
+    description = models.TextField(verbose_name='description', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
