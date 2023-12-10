a07ae37a495591d45870dc33e9918707804b5ae0
Aleksei Ponomarenko <lehapon92@gmail.com>
2023-12-11 00:51:18 +0900

    HW 21.1 1st task complited

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from catalog.models import Product, Category
 
 
-def home(request):
-    product_list = Product.objects.all()
-    context = {
-        'object_list': product_list
-    }
-    return render(request, 'catalog/home.html', context)
+# def home(request):
+#     product_list = Product.objects.all()
+#     context = {
+#         'object_list': product_list
+#     }
+#     return render(request, 'catalog/home.html', context)
+
+
+class HomeListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
 
 
 def contacts(request):
@@ -19,8 +26,12 @@ def contacts(request):
     return render(request, 'catalog/contacts.html')
 
 
-def product(request, pk):
-    context = {
-        'object': Product.objects.get(pk=pk)
-    }
-    return render(request, 'catalog/product.html', context)
+# def product(request, pk):
+#     context = {
+#         'object': Product.objects.get(pk=pk)
+#     }
+#     return render(request, 'catalog/product.html', context)
+
+
+class ProductDetailView(DetailView):
+    model = Product
